851a28e1a90874251111e1bf9672027baa7d4097
“tastethedream” <tastethedream@gmail.com>
2021-10-13 19:41:06 +0100

    theme changed and created home.dart

--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+
+// 1
+import 'fooderlich_theme.dart';
 
 void main() {
-  // 1
   runApp(const Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
-  // 2
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
+    // 2
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
-    // 3
     return MaterialApp(
-      // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
-      // 4
-      home: Scaffold(
-        // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
-        // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
-      ),
+      home: const Home(),
     );
+
   }
-}
