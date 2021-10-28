2b0787c09205fdcaab142b3d04eff4cbb5337a82
“tastethedream” <tastethedream@gmail.com>
2021-10-28 20:03:49 +0100

    gesture detectors added

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_list_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_list_screen.dart
@@ -27,17 +27,61 @@ class GroceryListScreen extends StatelessWidget {
           final item = groceryItems[index];
           // TODO 28: Wrap in a Dismissable
           // TODO 27: Wrap in an InkWell
+
           // 5
-          return GroceryTile(
-            key: Key(item.id),
-            item: item,
+          // 1
+          return Dismissible(
             // 6
-            onComplete: (change) {
-              // 7
-              if (change != null) {
-                manager.completeItem(index, change);
-              }
-            },);
+            key: Key(item.id),
+            // 7
+            direction: DismissDirection.endToStart,
+            // 8
+            background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                child: const Icon(Icons.delete_forever,
+                    color: Colors.white, size: 50.0)),
+            // 9
+            onDismissed: (direction) {
+              // 10
+              manager.deleteItem(index);
+              // 11
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('${item.name} dismissed')));
+            },
+            child:
+            InkWell(
+              child: GroceryTile(
+                  key: Key(item.id),
+                  item: item,
+                  onComplete: (change) {
+                    if (change != null) {
+                      manager.completeItem(index, change);
+                    }
+                  }),
+              // 2
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GroceryItemScreen(
+                      originalItem: item,
+                      // 3
+                      onUpdate: (item) {
+                        // 4
+                        manager.updateItem(item, index);
+                        // 5
+                        Navigator.pop(context);
+                      },
+                      // 6
+                      onCreate: (item) {},
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+
         },
         // 8
         separatorBuilder: (context, index) {
