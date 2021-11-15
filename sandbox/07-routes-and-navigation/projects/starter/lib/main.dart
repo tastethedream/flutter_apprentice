7ca090192af545105c6c8653267c4bd5004089b9
“tastethedream” <tastethedream@gmail.com>
2021-11-15 10:03:05 +0000

    test environment added to run labs

---
+++ sandbox/07-routes-and-navigation/projects/starter/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'fooderlich_theme.dart';
+import 'models/models.dart';
+// TODO: Import app_router
+import 'navigation/app_router.dart';
+
+
+void main() {
+  runApp(
+    const Fooderlich(),
+  );
+}
+
+class Fooderlich extends StatefulWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  _FooderlichState createState() => _FooderlichState();
+}
+
+class _FooderlichState extends State<Fooderlich> {
+  final _groceryManager = GroceryManager();
+  final _profileManager = ProfileManager();
+  // TODO: Create AppStateManager
+  final _appStateManager = AppStateManager();
+
+  // TODO: Define AppRouter
+  late AppRouter _appRouter;
+
+
+  // TODO: Initialize app router
+  @override
+  void initState() {
+    _appRouter = AppRouter(
+      appStateManager: _appStateManager,
+      groceryManager: _groceryManager,
+      profileManager: _profileManager,
+    );
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => _groceryManager,
+        ),
+        ChangeNotifierProvider(
+          create: (context) => _profileManager,
+        ),
+        // TODO: Add AppStateManager ChangeNotifierProvider
+        ChangeNotifierProvider(create: (context) => _appStateManager),
+
+      ],
+      child: Consumer<ProfileManager>(
+        builder: (context, profileManager, child) {
+          ThemeData theme;
+          if (profileManager.darkMode) {
+            theme = FooderlichTheme.dark();
+          } else {
+            theme = FooderlichTheme.light();
+          }
+
+          return MaterialApp(
+            theme: theme,
+            title: 'Fooderlich',
+            // TODO: Replace with Router widget
+            home: Router(
+              routerDelegate: _appRouter,
+              // TODO: Add backButtonDispatcher
+            ),
+
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ sandbox/07-routes-and-navigation/projects/starter/lib/navigation/app_router.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import '../screens/screens.dart';
+
+// 1
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  // 2
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  // 3
+  final AppStateManager appStateManager;
+  // 4
+  final GroceryManager groceryManager;
+  // 5
+  final ProfileManager profileManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  })
+      : navigatorKey = GlobalKey<NavigatorState>() {
+    // TODO: Add Listeners
+    appStateManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+
+  }
+
+  // TODO: Dispose listeners
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    super.dispose();
+  }
+
+
+  // 6
+  @override
+  Widget build(BuildContext context) {
+    // 7
+    return Navigator(
+      // 8
+      key: navigatorKey,
+      // TODO: Add onPopPage
+      onPopPage: _handlePopPage,
+
+      // 9
+      pages: [
+        // TODO: Add SplashScreen
+        if (!appStateManager.isInitialized) SplashScreen.page(),
+
+        // TODO: Add LoginScreen
+        if (appStateManager.isInitialized && !appStateManager.isLoggedIn)
+          LoginScreen.page(),
+
+        // TODO: Add OnboardingScreen
+        if (appStateManager.isLoggedIn &&
+            !appStateManager.isOnboardingComplete)
+          OnboardingScreen.page(),
+
+        // TODO: Add Home
+        if (appStateManager.isOnboardingComplete)
+          Home.page(appStateManager.getSelectedTab),
+
+        // TODO: Create new item
+        // 1
+        if (groceryManager.isCreatingNewItem)
+// 2
+          GroceryItemScreen.page(
+            onCreate: (item) {
+              // 3
+              groceryManager.addItem(item);
+            }, onUpdate: (item, index) {
+            // 4 No update
+          },
+          ),
+
+        // TODO: Select GroceryItemScreen
+        // 1
+        if (groceryManager.selectedIndex != -1)
+        // 2
+          GroceryItemScreen.page(
+              item: groceryManager.selectedGroceryItem,
+              index: groceryManager.selectedIndex,
+              onUpdate: (item, index) {
+                // 3
+                groceryManager.updateItem(item, index);
+              },
+              onCreate: (_) {
+                // 4 No create
+              }
+          ),
+
+        // TODO: Add Profile Screen
+        if (profileManager.didSelectUser)
+          ProfileScreen.page(profileManager.getUser),
+
+        // TODO: Add WebView Screen
+      ],
+    );
+  }
+
+  // TODO: Add _handlePopPage
+  bool _handlePopPage(
+      // 1
+      Route<dynamic> route,
+      // 2
+      result) {
+    // 3
+    if (!route.didPop(result)) {
+      // 4
+      return false;
+    }
+
+    // 5
+    // TODO: Handle Onboarding and splash
+    if (route.settings.name == FooderlichPages.onboardingPath) {
+      appStateManager.logout();
+    }
+
+    // TODO: Handle state when user closes grocery item screen
+    if (route.settings.name == FooderlichPages.groceryItemDetails) {
+      groceryManager.groceryItemTapped(-1);
+    }
+
+    // TODO: Handle state when user closes profile screen
+    // TODO: Handle state when user closes WebView screen
+    // 6
+    return true;
+  }
+
+
+  // 10
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
