60a930bb4ff370d25f103d4c34f5b6ea3e55022f
“tastethedream” <tastethedream@gmail.com>
2021-12-14 11:10:54 +0000

    chat function and storage working

---
+++ 19-firebase-realtime-database/projects/starter/lib/data/message_dao.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'message.dart';
+
+class MessageDao {
+  // 1
+  final CollectionReference collection =
+  FirebaseFirestore.instance.collection('messages');
+// TODO: Add saveMessage
+  void saveMessage(Message message) {
+    collection.add(message.toJson());
+  }
+// TODO: Add getMessageStream
+  Stream<QuerySnapshot> getMessageStream() {
+    return collection.snapshots();
+  }
+
+
+}
--- 19-firebase-realtime-database/projects/starter/lib/ui/message_list.dart
+++ 19-firebase-realtime-database/projects/starter/lib/ui/message_list.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../data/message.dart';
+import '../data/message_dao.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'message_widget.dart';
 
 
 class MessageList extends StatefulWidget {
@@ -18,6 +23,8 @@ class MessageListState extends State<MessageList> {
   Widget build(BuildContext context) {
     WidgetsBinding.instance!.addPostFrameCallback((_) => _scrollToBottom());
     // TODO: Add MessageDao
+    final messageDao = Provider.of<MessageDao>(context, listen: false);
+
 
     // TODO: Add UserDao
 
@@ -31,7 +38,8 @@ class MessageListState extends State<MessageList> {
         child: Column(
           children: [
             // TODO: Add Message DAO to _getMessageList
-            _getMessageList(),
+            _getMessageList(messageDao),
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -43,7 +51,8 @@ class MessageListState extends State<MessageList> {
                       controller: _messageController,
                       onSubmitted: (input) {
                         // TODO: Add Message DAO 1
-                        _sendMessage();
+                        _sendMessage(messageDao);
+
                       },
                       decoration:
                           const InputDecoration(hintText: 'Enter new message'),
@@ -56,7 +65,8 @@ class MessageListState extends State<MessageList> {
                         : CupertinoIcons.arrow_right_circle),
                     onPressed: () {
                       // TODO: Add Message DAO 2
-                      _sendMessage();
+                      _sendMessage(messageDao);
+
                     })
               ],
             ),
@@ -67,17 +77,67 @@ class MessageListState extends State<MessageList> {
   }
 
   // TODO: Replace _sendMessage
-  void _sendMessage() {
+  void _sendMessage(MessageDao messageDao) {
+    if (_canSendMessage()) {
+      final message = Message(
+        text: _messageController.text,
+        date: DateTime.now(),
+        // TODO: add email
+      );
+      messageDao.saveMessage(message);
+      _messageController.clear();
+      setState(() {});
+    }
   }
+
 
   // TODO: Replace _getMessageList
-  Widget _getMessageList() {
-    return const SizedBox.shrink();
+  Widget _getMessageList(MessageDao messageDao) {
+    return Expanded(
+      // 1
+      child: StreamBuilder<QuerySnapshot>(
+        // 2
+        stream: messageDao.getMessageStream(),
+        // 3
+        builder: (context, snapshot) {
+          // 4
+          if (!snapshot.hasData)
+            return const Center(child: LinearProgressIndicator());
+
+          // 5
+          return _buildList(context, snapshot.data!.docs);
+        },
+      ),
+    );
   }
 
+
   // TODO: Add _buildList
+  Widget _buildList(BuildContext context, List<DocumentSnapshot>? snapshot) {
+    // 1
+    return ListView(
+      controller: _scrollController,
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.only(top: 20.0),
+      // 2
+      children: snapshot!.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+
 
   // TODO: Add _buildListItem
+  Widget _buildListItem(BuildContext context, DocumentSnapshot snapshot) {
+    // 1
+    final message = Message.fromSnapshot(snapshot);
+    // 2
+    return MessageWidget(
+        message.text,
+        message.date,
+        message.email
+    );
+  }
+
 
   bool _canSendMessage() => _messageController.text.length > 0;
 
